937d4caeb5192e6f76fab1154f6750435ea401c6
Raam <raam@singularitynet.io>
2021-04-29 22:30:34 +0530

    Order by descending balance

--- token_transfer.py
+++ token_transfer.py
@@ -20,7 +20,7 @@ class TokenTransfer(BlockchainHandler):
         self._agi_handler = AGITokenHandler(ws_provider)
         self._contract_name = 'TokenBatchTransfer'
         self._query = 'SELECT * from token_snapshots where balance_in_cogs > 0 and wallet_address not in '  + \
-                       '(SELECT wallet_address from transfer_info where transfer_status = \'SUCCESS\') '
+                       '(SELECT wallet_address from transfer_info where transfer_status = \'SUCCESS\' and is_contract = 0)  order by balance_in_cogs desc'
         self._insert = 'INSERT INTO transfer_info ' + \
         '(wallet_address, transfer_fees, transfer_time, transfer_transaction, transfer_status, transfer_amount_in_cogs, row_created, row_updated) ' + \
         'VALUES (%s, 0, current_timestamp, %s, %s, %s, current_timestamp, current_timestamp) '
