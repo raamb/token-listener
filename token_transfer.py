2e3285c9110b87c6ea387862be4a1766a7097f3b
Raam <raam@singularitynet.io>
2021-04-29 08:29:59 +0530

    Added dry run mode for transfers

--- token_transfer.py
+++ token_transfer.py
@@ -15,7 +15,7 @@ COMMON_CNTRCT_PATH = os.path.abspath(
      
 #TODO POST DEPLOYMENT
 class TokenTransfer(BlockchainHandler):
-    def __init__(self, ws_provider, approve, deposit):
+    def __init__(self, ws_provider, net_id, dry_run):
         super().__init__(ws_provider)
         self._agi_handler = AGITokenHandler(ws_provider)
         self._contract_name = 'TokenBatchTransfer'
@@ -26,8 +26,10 @@ class TokenTransfer(BlockchainHandler):
         'VALUES (%s, 0, current_timestamp, %s, %s, %s, current_timestamp, current_timestamp) '
         # 'ON DUPLICATE KEY UPDATE balance_in_cogs = %s, row_updated = current_timestamp'
         self._repository = Repository()
-        self._approve = approve
-        self._deposit = deposit
+        self._dry_run = dry_run
+        self._net_id = net_id
+        self._approve = False
+        self._deposit = False
         self._balances = dict()
         self._batchsize = 100
         self._offset = 1
@@ -36,9 +38,9 @@ class TokenTransfer(BlockchainHandler):
         return os.path.abspath(
             os.path.join(os.path.dirname(__file__), 'node_modules', 'batch-token-transfer'))
     
-    def _get_contract_address(self, net_id):
+    def _get_contract_address(self):
         contract_network_path, contract_abi_path = self._blockchain_util. get_contract_file_paths(self._get_base_contract_path(), self._contract_name)
-        self._contract_address = self._blockchain_util.read_contract_address(net_id=net_id, path=contract_network_path, key='address')
+        self._contract_address = self._blockchain_util.read_contract_address(net_id=self._net_id, path=contract_network_path, key='address')
         return self._contract_address
 
     def _insert_transaction(self, transaction_hash):
@@ -46,17 +48,20 @@ class TokenTransfer(BlockchainHandler):
         transaction_data = []
         for address in self._balances:
             transaction_data.append([address,transaction_hash,'SUCCESS',str(self._balances[address])])
-        self._repository.bulk_query(self._insert, transaction_data)
+        if not self._dry_run:
+            self._repository.bulk_query(self._insert, transaction_data)
         print(f"{(time.process_time() - start)} seconds taken to insert transaction")
 
-    def _approve_deposit_funds(self,net_id):
-        address = self._get_contract_address(net_id)
+    def _approve_deposit_funds(self):
+        address = self._get_contract_address()
         if self._approve:
             print("Approving transfer for address " + str(address))
-            self._agi_handler.approve_transfer(address,TOTAL_COGS_TO_APPROVE, net_id)
+            if not self._dry_run:
+                self._agi_handler.approve_transfer(address,TOTAL_COGS_TO_APPROVE, self._net_id)
         if self._deposit:
             print("Transfer for address " + str(address))
-            self._agi_handler.deposit(address,TOTAL_COGS_TO_TRANSFER, net_id)
+            if not self._dry_run:
+                self._agi_handler.deposit(address,TOTAL_COGS_TO_TRANSFER, self._net_id)
 
     def _transfer_tokens(self):
         addresses = []
@@ -66,9 +71,13 @@ class TokenTransfer(BlockchainHandler):
             amounts.append(self._balances[address])
         
         positional_inputs = (addresses, amounts)
-        transaction_hash = self._make_trasaction(net_id, TRANSFERER_ADDRESS, TRANSFERER_PRIVATE_KEY, *positional_inputs, method_name="batchTransfer")
-        print(f"transaction hash {transaction_hash} generated for batchTransfer")
-        self._await_transaction(transaction_hash)
+        if not self._dry_run:
+            transaction_hash = self._make_trasaction(self._net_id, TRANSFERER_ADDRESS, TRANSFERER_PRIVATE_KEY, *positional_inputs, method_name="batchTransfer")
+            print(f"transaction hash {transaction_hash} generated for batchTransfer")
+            self._await_transaction(transaction_hash)
+        else:
+            transaction_hash = 'dry_run'
+            print(f"BATCH TRASNFER {positional_inputs}")
         return transaction_hash   
 
     def _transfer(self):
@@ -92,9 +101,9 @@ class TokenTransfer(BlockchainHandler):
         self._balances.clear()
         self._transfer()
     
-    def process_transfer(self,net_id):
-        print("Transferring for network " + str(net_id))
-        self._approve_deposit_funds(net_id)
+    def process_transfer(self):
+        print("Transferring for network " + str(self._net_id))
+        self._approve_deposit_funds()
         self._transfer()
 
 
@@ -102,16 +111,17 @@ def print_usage():
     print("USAGE: token_transfer.py -n <network_id>")
 
 argv = sys.argv[1:]
-if len(argv) >= 2:
+if len(argv) < 3:
     print_usage()
     sys.exit()
 
 try:
     snapshot_start = time.process_time()
-    opts, args = getopt.getopt(argv,"h:n:d:a",["input-file="])
+    opts, args = getopt.getopt(argv,"h:n:d",["input-file="])
     net_id = 3
     approve=False
     deposit=False
+    dry_run=False
     for opt, arg in opts:
         print(opt)
         if opt == '-h':
@@ -119,15 +129,15 @@ try:
             sys.exit()
         elif opt in ("-n", "--network_id"):
             net_id = int(arg)
-        elif opt in ("-a", "--approve"):
-            print("Approve")
-            approve = False
-        elif opt in ("-d", "--deposit"):
-            print("Deposit")
-            deposit = True
+        #elif opt in ("-a", "--approve"):
+        #    print("Approve")
+        #   approve = False
+        elif opt in ("-d", "--dry_run"):
+            print("DRY RUN MODE")
+            dry_run = True
     
-    t = TokenTransfer(INFURA_URL,approve, deposit)
-    t.process_transfer(net_id)
+    t = TokenTransfer(INFURA_URL,net_id,dry_run)
+    t.process_transfer()
     print(f"{(time.process_time() - snapshot_start)} seconds taken") 
 except getopt.GetoptError:
     print_usage()
